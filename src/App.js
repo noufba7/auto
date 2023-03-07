09314b9501e75ec5e511feaa7de6c5304acdc32d
Nouf mohammed <95718202+noufba7@users.noreply.github.com>
2023-03-07 19:26:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+// every element in app.js u must father elment
+
+function App() {
+  return (
+    //for style use classname
+<>
+    <h1>hay</h1>
+    <h2>hay</h2>
+</>
+  
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// the file get app.js to index.html
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
